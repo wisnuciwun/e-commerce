a86d378c4cccacb5c62c0b1ddaf2387001d4818c
wisnuciwun <adiwardhanawisnu@gmail.com>
2021-12-09 01:50:25 +0700

    :art: improve header

--- src/container/DefaultHeader.tsx
+++ src/container/DefaultHeader.tsx
@@ -6,10 +6,12 @@ class DefaultHeader extends PureComponent {
         return (
             <div className="header">
                 <MDBNavbar className="p-3">
-                    <div className="d-flex w-100">
+                    <div className="d-flex w-100 justify-content-between">
                         <MDBNavbarBrand className="default-color"><b>E-commerce</b></MDBNavbarBrand>
-                        <input style={{marginRight: '10px'}} className="form-control mr-3" type="text" placeholder="Search" aria-label="Search" />
-                        <MDBBtn><h5><i className="bi bi-search"></i></h5></MDBBtn>
+                        <div className="d-flex justify-content-center w-50">
+                            <input style={{ marginRight: '10px' }} className="form-control mr-3" type="text" placeholder="Search" aria-label="Search" />
+                            <MDBBtn><h5><i className="bi bi-search"></i></h5></MDBBtn>
+                        </div>
                     </div>
                 </MDBNavbar>
             </div>
